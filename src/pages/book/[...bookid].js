adbbd1dee1eb0a0e9de867f2b74fcd225876d2e0
bigscotia10 <chris.ferrell@pettygrovefull.com>
2023-04-30 20:29:31 -0700

    fix the book order

--- src/pages/book/[...bookid].js
+++ src/pages/book/[...bookid].js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import BookPage from '../components/BookPage';
 import { db } from '../../firebase'; // Import the Firestore instance
-import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
+import { collection, doc, getDoc, getDocs, orderBy, query } from 'firebase/firestore';
 
 export default function Book({ book }) {
     const router = useRouter();
@@ -28,8 +28,9 @@ export async function getServerSideProps({ params }) {
     const bookData = bookDoc.data();
 
     // Fetch pages subcollection data
-    const pagesQuery = await getDocs(collection(bookRef, 'pages'));
-    const pagesData = pagesQuery.docs.map(doc => doc.data());
+    const pagesQuery = query(collection(bookRef, 'pages'), orderBy('pageIndex', 'asc'));
+    const pagesSnapshot = await getDocs(pagesQuery);
+    const pagesData = pagesSnapshot.docs.map(doc => doc.data());
 
     // Add pages data to the book data object
     bookData.pages = pagesData;
