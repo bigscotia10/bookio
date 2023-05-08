d8c8cda44230aa0fbd213b39bf3504d32ba0710a
Markus Weldon <markus.weldon@gmail.com>
2023-05-08 16:43:43 -0700

    Nav usability

--- src/pages/components/Menu.js
+++ src/pages/components/Menu.js
@@ -4,13 +4,18 @@ import Link from 'next/link';
 const Menu = () => {
     return (
         <nav className="menu-nav">
-            <img src="/littlebean.png" alt="LITTLEBEAN Logo" className="logo-image" />
             <ul className="menu-ul">
                 <li className="menu-li">
                     <Link href="/" className="menu-a">
-                        Create Book
+                        Home
                     </Link>
                 </li>
+                <li className="menu-li">
+                    <Link href="/instructions" className="menu-a">
+                        Instructions (How to)
+                    </Link>
+                </li>
+
                 <li className="menu-li">
                     {/* <Link href="/all-books" className="menu-a">
                         All Books
